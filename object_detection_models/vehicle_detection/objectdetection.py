f38430fdd80958a2847776927b9ebf37a6f9e9bd
Fay <fychuttun27@pupils.nlcsjeju.kr>
2026-06-15 08:25:02 +0900

    added light detection model

--- object_detection_models/vehicle_detection/objectdetection.py
+++ object_detection_models/vehicle_detection/objectdetection.py
@@ -2,8 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 def load_model(weights_path):
-    model = YOLO(weights_path)
-    return model
+    return YOLO(weights_path)
 
 def run_detection(model, frame, conf=0.4):
     results = model.track(frame, conf=conf, tracker="bytetrack.yaml",verbose=False, persist=True)
@@ -15,6 +14,16 @@ def run_detection(model, frame, conf=0.4):
             x1, y1, x2, y2 = box.xyxy[0].tolist()
             boxes.append((x1, y1, x2, y2, int(track_id)))
     return boxes
+    
+def run_light_detection(model, frame, conf=0.3):
+    results = model(frame, conf=conf, verbose=False)
+    boxes = []
+    for r in results:
+        for box in r.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            boxes.append((x1, y1, x2, y2))
+    return boxes
+
 
 if __name__ == "__main__":
     model = load_model("runs/detect/train/weights/best.pt")
