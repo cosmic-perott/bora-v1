26afca7f899f8fbcc6f1a66f25414dc1871c1684
Fay <fychuttun27@pupils.nlcsjeju.kr>
2026-06-15 20:10:26 +0900

    indent

--- application.py
+++ application.py
@@ -134,11 +134,11 @@ def run():
             cv2.rectangle(dehazed,
                   (int(x1), int(y1)), (int(x2), int(y2)),
                   (0, 255, 0), 1)
-        light_boxes = run_light_detection(light_model, dehazed)
+    light_boxes = run_light_detection(light_model, dehazed)
 
-        for box in light_boxes:
-            x1, y1, x2, y2 = box[:4]
-            cv2.rectangle(dehazed, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 1)
+    for box in light_boxes:
+        x1, y1, x2, y2 = box[:4]
+        cv2.rectangle(dehazed, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 1)
             
         warning_level, primary_box = check_proximity(boxes, frame_w, frame_h)
 
